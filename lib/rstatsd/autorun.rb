53a56c885d60bf8ea92e94fee6b15675194d16ca
Dan Herrera <whoahbot@gmail.com>
2012-02-21 14:43:55 -0800

    adding in instructions that get printed after EM starts

--- lib/rstatsd/autorun.rb
+++ lib/rstatsd/autorun.rb
@@ -4,4 +4,6 @@ require_relative 'collector'
 EventMachine::run {
   EventMachine::open_datagram_socket("127.0.0.1", 8125, Rstatsd::Collector)
   EventMachine::start_server("127.0.0.1", 8126, Rstatsd::Server)
+  puts "http server running at http://localhost:8126/"
+  puts "go there for more instructions"
 }
